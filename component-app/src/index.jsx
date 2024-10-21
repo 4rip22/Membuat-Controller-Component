ff2efd8bbef8f1762aa84ee6d076f9f38377cbf0
4rip22 <kkruipputra816@gmail.com>
2024-10-21 23:35:14 +0700

    update src

--- component-app/src/index.jsx
+++ component-app/src/index.jsx
@@ -9,22 +9,46 @@ class MyForm extends React.Component{
             name: '',
             email: '',
             gender: 'Man'
-        }
+        };
+        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+        this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
+        this.onGenderChangeEventHandler = this.onGenderChangeEventHandler.bind(this);
     }
-    //inisialisasi state
+    onNameChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                name: event.target.value
+            }
+        })
+    }
+    onEmailChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                email: event.target.value
+            }
+        })
+    }
+    onGenderChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                gender: event.target.value
+            }
+        })
+    }
+
     render(){
         return(
             <div>
                 <h1>Register Form</h1>      
                 <form>
                     <label htmlFor="name">Name: </label>
-                    <input id="name" type="text" value={this.state.name}/>
+                    <input id="name" type="text" value={this.state.name} onChange={this.onNameChangeEventHandler}/>
                     <br/>
                     <label htmlFor="email">Email: </label>
-                    <input id="email" type="email" value={this.state.email}></input>
+                    <input id="email" type="email" value={this.state.email} onChange={this.onEmailChangeEventHandler}></input>
                     <br/>
                     <label htmlFor="gender">Gender: </label>
-                    <select  id="gender" value={this.state.gender}>
+                    <select  id="gender" value={this.state.gender} onChange={this.onGenderChangeEventHandler}>
                         <option value="Man">Man</option>
                         <option value="Woman">Woman</option>
                     </select>
